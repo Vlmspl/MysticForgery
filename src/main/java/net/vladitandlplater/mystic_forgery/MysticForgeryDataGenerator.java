f0814c5eebc1f267b2539dd1aeae96772e8cef7b
Владислав Білоус <bilousvlad.a@outlook.com>
2025-02-21 19:50:04 +0200

    Base

---
+++ src/main/java/net/vladitandlplater/mystic_forgery/MysticForgeryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vladitandlplater.mystic_forgery;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MysticForgeryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
