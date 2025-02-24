76270be0cd1faa2541d378083c6219f6fc4978af
Владислав Білоус <bilousvlad.a@outlook.com>
2025-02-24 20:56:14 +0200

    Added a file for further changing

---
+++ src/main/java/net/vladitandlplater/mystic_forgery/systems/attributes/LightingUpHitAttribute.java
@@ -0,0 +1,11 @@
+package net.vladitandlplater.mystic_forgery.systems.attributes;
+
+/**
+ * For clarity, I must say, it lights up the area around the hit zone.
+ */
+public class LightingUpHitAttribute extends OnHitAttribute{
+    @Override
+    public void OnHitAction() {
+
+    }
+}
